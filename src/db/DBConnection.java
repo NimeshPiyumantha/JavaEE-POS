d0c20bf8c99ebc4876cd3c665a81bfc924246f34
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-04 18:09:02 +0530

    Add db and model customer

---
+++ src/db/DBConnection.java
@@ -0,0 +1,32 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    // rule 1
+    private static DBConnection dbConnection=null;
+    private final Connection connection;
+
+    // rule 2
+    private DBConnection() throws ClassNotFoundException, SQLException {
+        //create new connection
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ThogaKade", "root", "1234");
+
+    }
+
+    // rule 3
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+//        if (dbConnection == null) {
+//            dbConnection = new DBConnection();
+//        }
+//        return dbConnection;
+        return (dbConnection==null)?dbConnection = new DBConnection():dbConnection;
+    }
+
+    public Connection getConnection() {
+        return connection;//DBConnection.getInstance().getConnection();
+    }
+}
---
+++ src/model/CustomerDTO.java
@@ -0,0 +1,15 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+    private String id;
+    private String name;
+    private String address;
+    private double salary;
+}
