22ffdd7b633f41057d83ea203e67688e0fff10e0
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-04 18:09:43 +0530

    Add Crud Util

---
+++ src/util/CrudUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute(String sql, Object...params) throws SQLException, ClassNotFoundException {
+        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement(sql);
+        for (int i = 0; i < params.length; i++) {
+            statement.setObject((i + 1), params[i]);
+        }
+        if (sql.startsWith("SELECT")) {
+            return (T) statement.executeQuery();
+        } else {
+            return ((T) (Boolean) (statement.executeUpdate() > 0));//boolean -> Boolean (Boxing) //type inference
+        }
+    }
+}
+
