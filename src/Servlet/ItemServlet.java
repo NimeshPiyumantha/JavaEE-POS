20c2cda2935030035de33dc9746bc2313cb9bd6c
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-05 17:05:29 +0530

    Add ItemServlet

---
+++ src/Servlet/ItemServlet.java
@@ -0,0 +1,26 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
