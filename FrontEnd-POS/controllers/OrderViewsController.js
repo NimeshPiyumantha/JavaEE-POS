ae94600c1a311a639dcadb9a42d55b0fbe0d2335
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-25 10:34:12 +0530

    Refactor Order Details

--- FrontEnd-POS/controllers/OrderViewsController.js
+++ FrontEnd-POS/controllers/OrderViewsController.js
@@ -3,24 +3,53 @@
  * @since : 0.1.0
  **/
 
+let baseUrl = "http://localhost:8080/Pos_JavaEE/";
+loadAllOrders();
+loadAllOrderDetails();
+
 function loadAllOrders() {
-
     $("#tblOrder").empty();
+    $.ajax({
+        url: baseUrl + "orders?option=LoadOrders", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
 
-    for (var order of orders) {
-        console.log(order);
-        var row = `<tr><td>${order.oId}</td><td>${order.cId}</td><td>${order.oDate}</td><td>${order.subTotal}</td><td>${order.discount}</td></tr>`;
-        $("#tblOrder").append(row);
-    }
+            for (let i of res.data) {
+                let orderId = i.orderId;
+                let date = i.date;
+                let cusId = i.cusId;
+
+                let row = "<tr><td>" + orderId + "</td><td>" + date + "</td><td>" + cusId + "</td></tr>";
+                $("#tblOrder").append(row);
+            }
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+
+    });
 }
 
 function loadAllOrderDetails() {
-
     $("#tblOrderDetails").empty();
+    $.ajax({
+        url: baseUrl + "orders?option=LoadOrderDetails", method: "GET", dataType: "json", success: function (res) {
+            console.log(res);
 
-    for (var orderDetail of orderDetails) {
-        console.log(orderDetail);
-        var row = `<tr><td>${orderDetail.orderId}</td><td>${orderDetail.cusId}</td><td>${orderDetail.itemId}</td><td>${orderDetail.qty}</td><td>${orderDetail.total}</td></tr>`;
-        $("#tblOrderDetails").append(row);
-    }
+            for (let i of res.data) {
+                let OrderId = i.OrderId;
+                let code = i.code;
+                let qty = i.qty;
+                let unitPrice = i.unitPrice;
+
+                let row = "<tr><td>" + OrderId + "</td><td>" + code + "</td><td>" + qty + "</td><td>" + unitPrice + "</td></tr>";
+                $("#tblOrderDetails").append(row);
+            }
+            console.log(res.message);
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            console.log(message);
+        }
+
+    });
 }
