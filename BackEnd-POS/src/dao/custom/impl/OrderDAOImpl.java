31e4bee6f803981065b1f4fc2c39e6355f7d72ae
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 23:18:44 +0530

    implement methodes to OrderDAOImpl,OrderDetailsDAOImpl

--- BackEnd-POS/src/dao/custom/impl/OrderDAOImpl.java
+++ BackEnd-POS/src/dao/custom/impl/OrderDAOImpl.java
@@ -1,10 +1,44 @@
 package dao.custom.impl;
 
 import dao.custom.OrderDAO;
+import entity.Orders;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class OrderDAOImpl implements OrderDAO {
+    @Override
+    public ArrayList<Orders> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(Orders dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Orders dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Orders> searchId(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
--- BackEnd-POS/src/dao/custom/impl/OrderDetailsDAOImpl.java
+++ BackEnd-POS/src/dao/custom/impl/OrderDetailsDAOImpl.java
@@ -1,10 +1,44 @@
 package dao.custom.impl;
 
 import dao.custom.OrderDetailsDAO;
+import entity.OrderDetail;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class OrderDetailsDAOImpl implements OrderDetailsDAO {
+    @Override
+    public ArrayList<OrderDetail> getAll(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean save(OrderDetail dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean update(OrderDetail dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public ArrayList<OrderDetail> searchId(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewID(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
