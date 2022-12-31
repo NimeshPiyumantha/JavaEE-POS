909b891fcdfb0a79d9f17c51b85e6401aca90561
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 13:10:56 +0530

    Add SuperDAO marker interface and CrudDAO interface

---
+++ BackEnd-POS/src/dao/CrudDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public interface CrudDAO<T, ID> extends SuperDAO {
+}
---
+++ BackEnd-POS/src/dao/SuperDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public interface SuperDAO {
+}
