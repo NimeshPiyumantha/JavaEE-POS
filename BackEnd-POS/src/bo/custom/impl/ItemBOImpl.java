c6c9e7aa89e8f8536c3ebcfe5191205dabc06ca2
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 17:16:45 +0530

    Refactor ItemDAOImpl,ItemDAO

--- BackEnd-POS/src/bo/custom/impl/ItemBOImpl.java
+++ BackEnd-POS/src/bo/custom/impl/ItemBOImpl.java
@@ -1,7 +1,10 @@
 package bo.custom.impl;
 
 import bo.custom.ItemBO;
+import dao.DAOFactory;
+import dao.custom.ItemDAO;
 import dto.ItemDTO;
+import entity.Item;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,33 +15,47 @@ import java.util.ArrayList;
  * @since : 0.1.0
  **/
 public class ItemBOImpl implements ItemBO {
+
+    private final ItemDAO itemDAO = (ItemDAO) DAOFactory.getDaoFactory().getDAO(DAOFactory.DAOTypes.ITEM);
+
     @Override
     public ArrayList<ItemDTO> getAllItems(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<Item> allI = itemDAO.getAll(connection);
+        ArrayList<ItemDTO> allItem = new ArrayList<>();
+
+        for (Item item : allI) {
+            allItem.add(new ItemDTO(item.getCode(), item.getDescription(), item.getQty(), item.getUnitPrice()));
+        }
+        return allItem;
     }
 
     @Override
     public boolean deleteItem(String code, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        return itemDAO.delete(code, connection);
     }
 
     @Override
     public boolean saveItem(ItemDTO dto, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        return itemDAO.save(new Item(dto.getCode(), dto.getDescription(), dto.getQty(), dto.getUnitPrice()), connection);
     }
 
     @Override
     public boolean updateItem(ItemDTO dto, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        return itemDAO.update(new Item(dto.getCode(), dto.getDescription(), dto.getQty(), dto.getUnitPrice()), connection);
     }
 
     @Override
     public String generateNewItemCode(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        return itemDAO.generateNewID(connection);
     }
 
     @Override
     public ArrayList<ItemDTO> itemSearchId(String id, Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ArrayList<Item> all = itemDAO.searchId(id, connection);
+        ArrayList<ItemDTO> allItem = new ArrayList<>();
+        for (Item item : all) {
+            allItem.add(new ItemDTO(item.getCode(), item.getDescription(), item.getQty(), item.getUnitPrice()));
+        }
+        return allItem;
     }
 }
--- BackEnd-POS/src/dao/custom/impl/ItemDAOImpl.java
+++ BackEnd-POS/src/dao/custom/impl/ItemDAOImpl.java
@@ -2,8 +2,10 @@ package dao.custom.impl;
 
 import dao.custom.ItemDAO;
 import entity.Item;
+import util.CrudUtil;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -14,31 +16,47 @@ import java.util.ArrayList;
 public class ItemDAOImpl implements ItemDAO {
     @Override
     public ArrayList<Item> getAll(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet result = CrudUtil.execute(connection, "SELECT * FROM Item");
+        ArrayList<Item> obList = new ArrayList<>();
+        while (result.next()) {
+            obList.add(new Item(result.getString(1), result.getString(2), result.getInt(3), result.getDouble(4)));
+        }
+        return obList;
     }
 
     @Override
     public boolean save(Item dto, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        return CrudUtil.execute(connection, "INSERT INTO Item VALUES (?,?,?,?)", dto.getCode(), dto.getDescription(), dto.getQty(), dto.getUnitPrice());
     }
 
     @Override
     public boolean update(Item dto, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+        return CrudUtil.execute(connection, "UPDATE Item SET description= ? , qty=? , unitPrice=? WHERE code=?", dto.getDescription(), dto.getQty(), dto.getUnitPrice(), dto.getCode());
     }
 
     @Override
-    public ArrayList<Item> searchId(String id, Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+    public ArrayList<Item> searchId(String code, Connection connection) throws SQLException, ClassNotFoundException {
+        ResultSet result = CrudUtil.execute(connection, "SELECT * FROM Item WHERE code=?", code);
+
+        ArrayList<Item> allItems = new ArrayList<>();
+        while (result.next()) {
+            allItems.add(new Item(result.getString(1), result.getString(2), result.getInt(3), result.getDouble(4)));
+        }
+        return allItems;
     }
 
     @Override
-    public boolean delete(String s, Connection connection) throws SQLException, ClassNotFoundException {
-        return false;
+    public boolean delete(String code, Connection connection) throws SQLException, ClassNotFoundException {
+        return CrudUtil.execute(connection, "DELETE FROM Item WHERE code=?", code);
     }
 
     @Override
     public String generateNewID(Connection connection) throws SQLException, ClassNotFoundException {
-        return null;
+        ResultSet result = CrudUtil.execute(connection, "SELECT code FROM Item ORDER BY code DESC LIMIT 1");
+        if (result.next()) {
+            return result.getString(1);
+        } else {
+            return null;
+        }
     }
 }
