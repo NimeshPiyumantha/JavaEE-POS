784c1dcc9636c5bfb536a31e8a7b6ba97ef904d2
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 13:58:09 +0530

    Implement customerBOImpl class

--- BackEnd-POS/src/bo/custom/impl/CustomerBOImpl.java
+++ BackEnd-POS/src/bo/custom/impl/CustomerBOImpl.java
@@ -1,10 +1,44 @@
 package bo.custom.impl;
 
 import bo.custom.CustomerBO;
+import dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public class CustomerBOImpl implements CustomerBO {
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public boolean saveCustomer(CustomerDTO dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDTO dto, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomer(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return false;
+    }
+
+    @Override
+    public String generateNewCustomerID(Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public ArrayList<CustomerDTO> customerSearchId(String id, Connection connection) throws SQLException, ClassNotFoundException {
+        return null;
+    }
 }
