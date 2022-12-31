aaebf0f60fd9d940693b338028ac2f62d64854b1
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 13:16:41 +0530

    Add CrudDAO interface to main crud opertion and logices

--- BackEnd-POS/src/dao/CrudDAO.java
+++ BackEnd-POS/src/dao/CrudDAO.java
@@ -1,8 +1,24 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * @author : Nimesh Piyumantha
  * @since : 0.1.0
  **/
 public interface CrudDAO<T, ID> extends SuperDAO {
+    ArrayList<T> getAll(Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean save(T dto, Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean update(T dto, Connection connection) throws SQLException, ClassNotFoundException;
+
+    ArrayList<T> searchId(String id, Connection connection) throws SQLException, ClassNotFoundException;
+
+    boolean delete(ID id, Connection connection) throws SQLException, ClassNotFoundException;
+
+    String generateNewID(Connection connection) throws SQLException, ClassNotFoundException;
+
 }
